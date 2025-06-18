8d488b2bcc8db00f7f747857ffbeff7088f8cc07
Drakonis96 <41027003+Drakonis96@users.noreply.github.com>
2025-06-18 22:23:34 +0200

    Update users tab behavior

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ werkzeug_logger.setLevel(logging.WARNING)
 # APPLICATION INFO
 # --------------------------------------------------------------------------- #
 APP_NAME = "PlexyTrack"
-APP_VERSION = "v0.2.7"
+APP_VERSION = "v0.3.0-experimental"
 USER_AGENT = f"{APP_NAME} / {APP_VERSION}"
 
 # --------------------------------------------------------------------------- #
